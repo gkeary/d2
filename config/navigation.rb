ef83761e9a6ecffcf507a4495bda3225f1f2d506
Gregg Keary <gregg@gkeary.com>
2011-11-12 08:17:42 -0500

    removed currend* files
    	deleted:    app/assets/javascripts/currend_day_routes.js.coffee
    	deleted:    app/assets/stylesheets/currend_day_routes.css.scss
    	deleted:    app/controllers/currend_day_routes_controller.rb
    	deleted:    app/helpers/currend_day_routes_helper.rb
    	deleted:    app/models/currend_day_route.rb
    	deleted:    app/views/currend_day_routes/_form.html.erb
    	deleted:    app/views/currend_day_routes/edit.html.erb
    	deleted:    app/views/currend_day_routes/index.html.erb
    	deleted:    app/views/currend_day_routes/new.html.erb
    	deleted:    app/views/currend_day_routes/show.html.erb
    	modified:   config/navigation.rb
    	modified:   config/routes.rb
    	deleted:    db/migrate/20111103030521_create_currend_day_routes.rb
    	deleted:    public/index.html
    	deleted:    spec/controllers/currend_day_routes_controller_spec.rb
    	deleted:    spec/helpers/currend_day_routes_helper_spec.rb
    	deleted:    spec/models/currend_day_route_spec.rb
    	deleted:    spec/requests/currend_day_routes_spec.rb
    	deleted:    spec/routing/currend_day_routes_routing_spec.rb
    	deleted:    spec/views/currend_day_routes/edit.html.erb_spec.rb
    	deleted:    spec/views/currend_day_routes/index.html.erb_spec.rb
    	deleted:    spec/views/currend_day_routes/new.html.erb_spec.rb
    	deleted:    spec/views/currend_day_routes/show.html.erb_spec.rb

--- config/navigation.rb
+++ config/navigation.rb
@@ -32,6 +32,26 @@ SimpleNavigation::Configuration.run do |navigation|
 
   # Define the primary navigation
   navigation.items do |primary|
+    primary.item :dispatch, 'Dispatch', dispatch_path
+  navigation.items do |primary|
+
+    primary.item :books, 'Books', books_path do |books|
+      books.item :fiction, 'Fiction', fiction_books_path
+      books.item :history, 'History', history_books_path
+      books.item :sports, 'Sports', sports_books_path
+    end
+    primary.item :music, 'Music', musics_path do |music|
+      music.item :rock, 'Rock', rock_musics_path
+      music.item :pop, 'Pop', pop_musics_path
+      music.item :alternative, 'Alternative', alternative_musics_path
+    end
+    primary.item :dvds, 'Dvds', dvds_path do |dvds|
+      dvds.item :drama, 'Drama', drama_dvds_path
+      dvds.item :action, 'Action', action_dvds_path
+      dvds.item :comedy, 'Comedy', comedy_dvds_path
+    end
+  end
+end
     # Add an item to the primary navigation. The following params apply:
     # key - a symbol which uniquely defines your navigation item in the scope of the primary_navigation
     # name - will be displayed in the rendered navigation. This can also be a call to your I18n-framework.
@@ -49,12 +69,12 @@ SimpleNavigation::Configuration.run do |navigation|
     #                            when the item should be highlighted, you can set a regexp which is matched
     #                            against the current URI.  You may also use a proc, or the symbol <tt>:subpath</tt>. 
     #
-    primary.item :key_1, 'name', url, options
+#    primary.item :key_1, 'name', url, options
 
     # Add an item which has a sub navigation (same params, but with block)
-    primary.item :key_2, 'name', url, options do |sub_nav|
+#    primary.item :key_2, 'name', url, options do |sub_nav|
       # Add an item to the sub navigation (same params again)
-      sub_nav.item :key_2_1, 'name', url, options
+#      sub_nav.item :key_2_1, 'name', url, options
     end
 
     # You can also specify a condition-proc that needs to be fullfilled to display an item.
